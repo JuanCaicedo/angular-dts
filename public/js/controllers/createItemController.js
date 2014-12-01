97c47315b0ce81c1f8ef1b42d72daba2087952f4
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-12-01 16:06:39 -0500

    creating works on the front end

--- public/js/controllers/createItemController.js
+++ public/js/controllers/createItemController.js
@@ -8,7 +8,14 @@ angular.module('dry-forms').controller('createItemController', [
         $scope.newItem = {};
 
         $scope.createItem = function(newItem) {
-            console.log('submitted item ' + newItem);
+            $http.post('/item', newItem)
+                .success(function(data, status, headers, config) {
+                    $scope.statusMessage = 'Your item has been created';
+                    console.log('created new item ' + newItem);
+                }).error(function(data, status, headers, config) {
+                    $scope.statusMessage = 'There was an error creating your item';
+                    console.log('error when creating new item');
+                });
         };
     }
 ]);
