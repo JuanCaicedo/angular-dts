71d1639524d20d8e36a8b2c4001a759c4901c7e9
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-12-02 14:32:04 -0500

    renamed html files

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var routes = require('./routes/index');
 
 var app = express();
 
-var Mongoose = require('mongoose');
-var db = Mongoose.createConnection('localhost', 'mytestapp');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
