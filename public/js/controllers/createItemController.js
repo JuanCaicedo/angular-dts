180fdb52fdb0d61fb612a524c8443c281075af88
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-12-02 10:56:45 -0500

    added results to create page

--- public/js/controllers/createItemController.js
+++ public/js/controllers/createItemController.js
@@ -7,11 +7,22 @@ angular.module('dry-forms').controller('createItemController', [
 
         $scope.newItem = {};
 
+        $http.get('/item').success(function(data, status, headers, config) {
+            if (data.items) {
+                $scope.items = data.items;
+            } else {
+                $scope.items = [];
+            }
+        });
+
         $scope.createItem = function(newItem) {
             $http.post('/item', newItem)
                 .success(function(data, status, headers, config) {
                     $scope.statusMessage = 'Your item has been created';
                     console.log('created new item ' + newItem);
+                    $scope.items.push(newItem);
+                    $scope.newItem = {};
+                    $scope.submitted = false;
                 }).error(function(data, status, headers, config) {
                     $scope.statusMessage = 'There was an error creating your item';
                     console.log('error when creating new item');
